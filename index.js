f0f44fbbd5a795c22f69325b1bdf3524e926a6cb
prashanth-devadas <prashanthharidas@yahoo.co.in>
2021-08-16 01:03:37 -0400

    weth dai pair, price check

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-import { ChainId, Token, Fetcher, WETH } from "@uniswap/sdk";
+import { ChainId, Token, Fetcher, WETH, Route, Trade, TokenAmount } from "@uniswap/sdk";
 
 const chainID = ChainId.MAINNET;
 const tokenAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
 const decimals = "18";
 const wethAddress = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
 
-const DAI = new Token(chainID, tokenAddress, decimals, "DAI");
-const weth = new Token(chainID, wethAddress, decimals, "WETH");
+const init = async () =>  {
+    const DAI = new Token(chainID, tokenAddress, decimals, "DAI");
+    const weth = WETH[chainID];
+    const pair = await Fetcher.fetchPairData(DAI, weth);
+    // const route = new Route([pair], weth)
+    const route = new Route([pair], weth);
 
+    console.log(route.midPrice.toSignificant(6));
+    console.log(route.midPrice.invert().toSignificant(6));
+    // const trade = new Trade(route, new TokenAmount(weth, '1000000000000000000'))
+}
 
-
+init();
